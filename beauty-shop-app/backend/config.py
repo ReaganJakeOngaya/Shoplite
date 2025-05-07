7a7f54c1088cfef74b86fb9bee94719bf50c0091
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2025-05-07 09:49:59 +0300

    Created SQLite DB

--- beauty-shop-app/backend/config.py
+++ beauty-shop-app/backend/config.py
@@ -1,4 +1,7 @@
 import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://localhost/beautyshop')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'beautyshop.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
